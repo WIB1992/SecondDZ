fe1dcf3b9fc46ef29a0019d076829e14f394b9bc
Александр <75897919+WIB1992@users.noreply.github.com>
2022-12-06 14:26:05 +0300

    Third commit

--- src/main/java/com/dmitriev/controller/EmployeeController.java
+++ src/main/java/com/dmitriev/controller/EmployeeController.java
@@ -20,7 +20,7 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public Employee showAllEmployees() {
-        Employee emp1 = new Employee("Vasili");
+        Employee emp1 = new Employee("Vasili Petrov");
         return emp1;
     }
     @GetMapping("/employees/{id}")
