380408fcfe1783fa08789527f46092f41bc6ec8f
Александр <75897919+WIB1992@users.noreply.github.com>
2022-12-06 15:16:22 +0300

    new commit

--- src/main/java/com/dmitriev/controller/EmployeeController.java
+++ src/main/java/com/dmitriev/controller/EmployeeController.java
@@ -20,7 +20,7 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public Employee showAllEmployees() {
-        Employee emp1 = new Employee("Vasili Petrov");
+        Employee emp1 = new Employee("Alex Petrov");
         return emp1;
     }
     @GetMapping("/employees/{id}")
